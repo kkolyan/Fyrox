dac318aa9dfb403eabc5a620f276d46b04fec69d
kkolyan <kkolyankkolyan@gmail.com>
2025-06-26 01:50:18 +0300

    allow to use build tool with executable that doesn't expect "--" as passthrough marker

--- project-manager/src/manager.rs
+++ project-manager/src/manager.rs
@@ -948,6 +948,7 @@ impl ProjectManager {
                 command: "cargo".to_string(),
                 args: vec!["clean".to_string()],
                 environment_variables: vec![],
+                skip_passthrough_marker: false,
             }]
             .into(),
             ui,
@@ -1049,6 +1050,7 @@ impl ProjectManager {
                 command: "cargo".to_string(),
                 args: vec!["update".to_string()],
                 environment_variables: vec![],
+                skip_passthrough_marker: false,
             },
         );
         self.run_selected_project_command(name, build_profile.build_and_run_queue(), ui);
